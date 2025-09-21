f9f09a00844e8aae48880214c2b78a69d60e31b1
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2025-09-21 18:58:22 +0200

    refacto: update app routes for movies & WIP watchlist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: '/movies',
     pathMatch: 'full',
   },
   {
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'movies',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/movies/movies.routes').then((m) => m.MOVIES_ROUTES),
   },
 ];
