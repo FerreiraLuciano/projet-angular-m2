42239023a6ae8701ae7d3b77c0c430da1725f103
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2025-09-21 19:00:40 +0200

    feat: added models for Movies

---
+++ src/app/features/movies/models/movie.model.ts
@@ -0,0 +1,25 @@
+export interface Movie {
+  id: number;
+  title: string;
+  original_title: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  poster_path?: string;
+  backdrop_path?: string;
+  posterUrl?: string;
+  backdropUrl?: string;
+  adult: boolean;
+  genre_ids: number[];
+  popularity: number;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  status?: 'to-watch' | 'watched'; // notre watchlist
+}
+
+export interface MovieRequest {
+  movie: Movie;
+  user: number;
+  status: 'watched' | 'to watch';
+}
